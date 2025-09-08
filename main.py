3e1cbfd1be7a5387044c7720a664deb9a2f060cd
Lucas <lucas.daniel@aluno.ufca.edu.br>
2025-09-08 01:13:47 -0300

    Personalidade Formal implementada

--- main.py
+++ main.py
@@ -14,34 +14,51 @@ def find_best_match(user_question: str, perguntas: list[str]) -> str | None:
     matches: list = get_close_matches(user_question, perguntas, n = 1, cutoff = 0.6)
     return matches[0] if matches else None
 
-def get_answer_for_question(pergunta: str, knowledge_base: dict) -> str | None:
+def resposta_para_pergunta(pergunta: str, knowledge_base: dict, tipo_resposta: str) -> str | None:
     for i in knowledge_base["perguntas"]:
-        if i["perguntas"] == pergunta:
-            return i["resposta"]
+        if i["pergunta"] == pergunta:
+            return i[tipo_resposta]
         
 def chat_bot():
     knowledge_base: dict = load_knowledge_base('knowledge_base.json')
 
     while True:
+
+        personalidade_resposta = int(input('1 - Formal\n2 - Orientador\n3 - Engraçado\nPersonalidade: '))
         user_input: str = input('You: ')
 
         if user_input.lower() == 'sair':
             break
+        
+        if personalidade_resposta == 1:
+            tipo_resposta = "resposta_formal"
+            best_match: str | None = find_best_match(user_input, [i["pergunta"] for i in knowledge_base["perguntas"]])
+        
+            if best_match:
+                personalidade_formal = Personalidades(knowledge_base, best_match, tipo_resposta)
+                personalidade_formal.resposta_formal()
+        
+            else:
+                print('TrampoJuá: Não sei te informar sobre isso. Você pode me ensinar?')
+                new_answer: str = input('Digite a resposta ou "pular" para pular: ')
 
-        best_match: str | None = find_best_match(user_input, [i["pergunta"] for i in knowledge_base["perguntas"]])
+                if new_answer.lower() != 'pular':
+                    knowledge_base["perguntas"].append({"pergunta": user_input, "resposta_formal": new_answer})
+                    save_knowledge_base('knowledge_base.json', knowledge_base)
+                    print('TrampoJuá: Obrigado! Aprendi uma nova informação!')
 
-        if best_match:
-            resposta =  get_answer_for_question(best_match, knowledge_base)
-            print(f"TrampoJuá: {resposta}")
-        else:
-            print('TrampoJuá: Não sei te informar sobre isso. Você pode me ensinar?')
-            new_answer: str = input('Digite a resposta ou "pular" para pular: ')
+class Personalidades:
+    def __init__(self, knowledge_base, best_match, tipo_resposta):
+        self.resposta = resposta_para_pergunta(best_match, knowledge_base, tipo_resposta)
+    def resposta_formal(self):
+        print(f"TrampoJuá (Formal): {self.resposta}")
+    def resposta_orientador(self):
+        print(f"TrampoJuá (Orientador): {self.resposta}")
 
-            if new_answer.lower() != 'pular':
-                knowledge_base["questions"].append({"question": user_input, "answer": new_answer})
-                save_knowledge_base('knowledge_base.json', knowledge_base)
-                print('TrampoJuá: Obrigado! Aprendi uma nova informação!')
 
 if __name__ == '__main__':
     chat_bot()
+
+
+
 
